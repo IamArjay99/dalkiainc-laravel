295073569a43cb914c8dbe8ce05d0dcf9658a13d
constarjaaay <constarjay@gmail.com>
2024-05-13 13:33:44 +0800

    Update Login title

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-    <title>Project Eagle | Login</title>
+    <title>Dalkia Inc. Website | Login</title>
     <meta content="" name="description">
     <meta content="" name="keywords">
 
