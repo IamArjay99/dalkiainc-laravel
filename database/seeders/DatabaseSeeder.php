88190452194225d68b591658edbdc09715428752
constarjay <constarjay@gmail.com>
2024-06-12 18:06:23 +0800

    Prepared data for deployment

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,21 +14,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // for ($i = 0; $i < 100; $i++) {
-        //     DB::table('projects')->insert([
-        //         'project_category_id' => rand(1, 4),
-        //         'name' => 'Project ' . rand(1, 100),
-        //         'client' => 'Client ' . rand(1, 100),
-        //         'description' => 'Description ' . rand(1, 100),
-        //         'floor' => 'Floor ' . rand(1, 100),
-        //         'scope_of_work' => 'Scope of work ' . rand(1, 100),
-        //         'image' => 'default.png',
-        //         'status' => ['ONGOING', 'COMPLETED'][rand(0, 1)],
-        //         'created_at' => now(),
-        //         'updated_at' => now()
-        //     ]);
-        // }
-
-        $this->call(UserSeeder::class);
+        $this->call(StartUpSeeder::class);
     }
 }
